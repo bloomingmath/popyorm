dfd292fb036566dacafa24142ff8b99a1d3986e7
Matteo Bortolotto <yamatteo@gmail.com>
2019-12-11 17:57:18 -0300

    More complex models

--- test_popy.py
+++ test_popy.py
@@ -10,10 +10,11 @@ def test_module_import():
 
 
 def test_extract_models():
-    d = extract_popy_models(fake_models)
-    assert isinstance(d, dict)
-    assert any(issubclass(m, Model) and m is not Model for key, m in d.items())
-    assert "Ma" in d.keys()
+    models_dict = extract_popy_models(fake_models)
+    assert isinstance(models_dict, dict)
+    assert any(issubclass(m, Model) and m is not Model for key, m in models_dict.items())
+    assert "Ma" in models_dict.keys()
+    assert hasattr(models_dict["ModelB"], "create_preparation")
 
 
 def test_generate_database():
@@ -21,40 +22,40 @@ def test_generate_database():
     assert hasattr(db, "Ma")
     db.Ma = getattr(db, "Ma")
     assert issubclass(db.Ma, db.Entity)
-    assert isinstance(getattr(db.Ma, "aa"), Required)
+    assert isinstance(getattr(db.Ma, "arg_a"), Required)
     assert isinstance(getattr(db.Ma, "id"), PrimaryKey)
 
 
 def test_generate_schemas():
     schemas = generate_schemas(fake_models)
     assert issubclass(schemas.create.ma, BaseModel)
-    assert "aa" in schemas.show.ma.__fields__
-    assert schemas.update.ma.__fields__["aa"].type_ == str
+    assert "arg_a" in schemas.show.ma.__fields__
+    assert schemas.update.ma.__fields__["arg_a"].type_ == str
 
 
 def test_generate_operations():
     operations = generate_operations(module=fake_models)
     assert callable(operations.create.ma)
     with db_session:
-        new_ima = operations.create.ma(db=db, create_info={"aa": "x"})
+        new_ima = operations.create.ma(db=db, create_info={"arg_a": "x"})
         assert isinstance(new_ima, db.Ma)
-        ima = operations.get.ma(db=db, get_info={"aa": "x"})
-        assert ima.aa == "x"
+        ima = operations.get.ma(db=db, get_info={"arg_a": "x"})
+        assert ima.arg_a == "x"
 
-        new_ima = operations.create.ma(db=db, create_info={"aa": "y"})
+        new_ima = operations.create.ma(db=db, create_info={"arg_a": "y"})
         query = operations.select.ma(db=db, select_info={})
         assert query.count() == 2
 
-        query = operations.select.ma(db=db, select_info={"aa": "x"})
+        query = operations.select.ma(db=db, select_info={"arg_a": "x"})
         assert query.count() == 1
 
-        query = operations.select.ma(db=db, select_info={"aa": "z"})
+        query = operations.select.ma(db=db, select_info={"arg_a": "z"})
         assert query.count() == 0
 
-        ima = operations.update.ma(db=db, get_info={"id": 1}, update_info={"aa": "z"})
-        assert ima.aa == "z"
-        assert operations.get.ma(db=db, get_info={"aa": "z"}).id == 1
+        ima = operations.update.ma(db=db, get_info={"id": 1}, update_info={"arg_a": "z"})
+        assert ima.arg_a == "z"
+        assert operations.get.ma(db=db, get_info={"arg_a": "z"}).id == 1
 
-        operations.delete.ma(db=db, get_info={"aa": "y"})
+        operations.delete.ma(db=db, get_info={"arg_a": "y"})
         assert operations.select.ma(db=db, select_info={}).count() == 1
 
