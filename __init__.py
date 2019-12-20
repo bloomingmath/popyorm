cb0fab8d6bd68bf0a16ba415e1c79cbaa45d93ab
Matteo Bortolotto <yamatteo@gmail.com>
2019-12-20 12:54:02 -0300

    Expose pony's TransactionIntegrityError

--- __init__.py
+++ __init__.py
@@ -1,4 +1,8 @@
-__all__ = ["ModelContainer", "Required", "Optional", "Set", "Json", "db_session", "Database"]
+__all__ = ["ModelContainer", "Required", "Optional", "Set", "Json", "db_session", "Database",
+           "TransactionIntegrityError"]
 
 from .popyorm import ModelContainer
 from pony.orm import Required, Optional, Set, Json, db_session, Database
+
+# TransactionIntegrityError is raised on attempt to create an instance that has a unique field with duplicate value
+from pony.orm.core import TransactionIntegrityError
